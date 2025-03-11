ce35d3b5db0e71b8f29aea317776df512cddd9da
avery <grimm4253@gmail.com>
2025-03-11 15:49:05 -0700

    NEW

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -7,14 +7,22 @@ class FormValidator {
     this._inputErrorClass = settings.inputErrorClass;
     this._inactiveButtonClass = settings.inactiveButtonClass;
     this._formEl = formEl;
+
+  
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    this._buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
   }
 
   _checkInputValidity(inputElement) {
     if (!inputElement) return;
+
     const errorElement = this._formEl.querySelector(
       `#${inputElement.id}-error`
     );
-
     if (!errorElement) {
       console.warn(`Error element for ${inputElement.id} not found.`);
       return;
@@ -32,8 +40,8 @@ class FormValidator {
   }
 
   _toggleButtonState() {
-    if (!this._inputList || !this._buttonElement) {
-      console.warn("FormValidator: Input list or submit button not found.");
+    if (!this._inputList.length || !this._buttonElement) {
+      console.warn("FormValidator: No inputs or submit button found.");
       return;
     }
 
@@ -49,13 +57,6 @@ class FormValidator {
   }
 
   _setEventListeners() {
-    this._inputList = Array.from(
-      this._formEl.querySelectorAll(this._inputSelector)
-    );
-    this._buttonElement = this._formEl.querySelector(
-      this._submitButtonSelector
-    );
-
     if (!this._inputList.length || !this._buttonElement) {
       console.warn("FormValidator: No inputs or button found.");
       return;
@@ -71,10 +72,20 @@ class FormValidator {
     });
   }
 
+  resetValidation() {
+    this._inputList.forEach((inputElement) => {
+      this._checkInputValidity(inputElement);
+    });
+
+    this._toggleButtonState();
+  }
+
   enableValidation() {
     this._formEl.addEventListener("submit", (evt) => {
       evt.preventDefault();
+      this.resetValidation();
     });
+
     this._setEventListeners();
   }
 }
